f6679c0909b9599362c3148e7c3f61c83275e737
Travis Nesland <tnesland@gmail.com>
2021-10-19 17:30:38 -0400

    fix: issues with linting and typescript configs (#15)

--- cypress/xo.config.cjs
+++ cypress/xo.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
   globals: ['cy'],
   rules: {
     ...parentConfig.rules,
+    '@typescript-eslint/triple-slash-reference': 0,
+    'import/extensions': 0,
+    'import/no-unassigned-import': 0,
     'max-nested-callbacks': 0,
+    'node/file-extension-in-import': 0,
   },
 }
--- xo.config.cjs
+++ xo.config.cjs
@@ -1,6 +1,25 @@
 module.exports = {
-  ignores: ['!examples/**'],
   prettier: true,
   semicolon: false,
   space: true,
+  plugins: ['jest'],
+  rules: {
+    'node/prefer-global/process': ['error', 'always'],
+    'jest/no-focused-tests': ['error'],
+    '@typescript-eslint/naming-convention': 'off',
+  },
+  overrides: [
+    {
+      files: '**/*.test.ts',
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 0,
+      },
+    },
+    {
+      files: 'next-env.d.ts',
+      rules: {
+        '@typescript-eslint/triple-slash-reference': 0,
+      },
+    },
+  ],
 }
